8c5d40b459ce562f9036399ab2b1f13423884514
Min Jun Sung <themrsung@gmail.com>
2025-09-01 22:42:32 +0900

    v1.0.4 - Item name fix

--- src/main/java/com/themrsung/mirae/command/admin/ItemNameCommand.java
+++ src/main/java/com/themrsung/mirae/command/admin/ItemNameCommand.java
@@ -4,7 +4,11 @@ import com.themrsung.mirae.MX;
 import com.themrsung.mirae.Mirae;
 import com.themrsung.mirae.account.Account;
 import com.themrsung.mirae.account.AccountTier;
+import com.themrsung.mirae.banknote.BanknoteQueryResult;
+import com.themrsung.mirae.banknote.Banknotes;
 import com.themrsung.mirae.command.MiraeCommand;
+import com.themrsung.mirae.item.CustomItem;
+import com.themrsung.mirae.item.economy.DonorCoin;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -49,6 +53,17 @@ public class ItemNameCommand extends MiraeCommand {
 
         ItemStack item = player.getInventory().getItemInMainHand();
         ItemMeta meta = item.getItemMeta();
+
+        BanknoteQueryResult bqr = Banknotes.isBanknote(item);
+        if (bqr.result() && !sender.isOp()) {
+            sender.sendMessage(Component.text("위조지폐를 생성할 수 없습니다.").style(MX.STYLE_WARNING));
+            return false;
+        }
+
+        if (CustomItem.DONOR_COIN.isItem(item) && !sender.isOp()) {
+            sender.sendMessage(Component.text("후원코인의 이름을 바꿀 수 없습니다.").style(MX.STYLE_WARNING));
+            return false;
+        }
 
         if (args.length < 1) {
             meta.displayName(null);
