355e89918075ffb7132d22be5459c9cd35cd74e6
Min Jun Sung <themrsung@gmail.com>
2025-09-10 19:48:46 +0900

    feat: boost wheat harvests with farming level

--- src/main/java/com/themrsung/mirae/listener/skill/SkillBoostListener.java
+++ src/main/java/com/themrsung/mirae/listener/skill/SkillBoostListener.java
@@ -7,6 +7,7 @@ import com.themrsung.mirae.skill.SkillType;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,6 +15,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Collection;
 
 /**
  * Skill boost listener.
@@ -46,6 +49,42 @@ public class SkillBoostListener implements Listener {
         e.setDropItems(false);
         Bukkit.getScheduler().runTaskLater(Mirae.getInstance(), () -> {
             block.getWorld().dropItem(block.getLocation(), itemsToDrop);
+        }, 1);
+    }
+
+    @EventHandler
+    public void onWheatHarvested(BlockBreakEvent e) {
+        if (e.isCancelled()) return;
+
+        Block block = e.getBlock();
+        if (block.getType() != Material.WHEAT) return;
+
+        Ageable age = (Ageable) block.getBlockData();
+        if (age.getAge() != age.getMaximumAge()) return;
+
+        Player player = e.getPlayer();
+        Account account = MX.requireAccountNonNull(Mirae.getState().getAccount(player));
+
+        long farmingSkillLevel = account.getSkillLevel(SkillType.FARMING);
+        long adjustedLevel = farmingSkillLevel - 30;
+        if (adjustedLevel < 0) return;
+
+        int additionalDrops = (int) Math.floorDiv(adjustedLevel, 20) + 1;
+        int finalDrops = Math.min(additionalDrops + 1, 16);
+
+        ItemStack tool = player.getInventory().getItemInMainHand();
+        Collection<ItemStack> drops = block.getDrops(tool, player);
+
+        ItemStack wheatToDrop = ItemStack.of(Material.WHEAT, finalDrops);
+
+        e.setDropItems(false);
+        Bukkit.getScheduler().runTaskLater(Mirae.getInstance(), () -> {
+            for (ItemStack drop : drops) {
+                if (drop.getType() != Material.WHEAT) {
+                    block.getWorld().dropItem(block.getLocation(), drop);
+                }
+            }
+            block.getWorld().dropItem(block.getLocation(), wheatToDrop);
         }, 1);
     }
 }
